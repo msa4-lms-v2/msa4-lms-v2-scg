3e93bec99114775f677d1f0682e9e4e7b95c6702
jinny-do <sonmojiri7@gmail.com>
2026-08-07 12:56:43 +0900

    first commit

---
+++ src/main/java/com/msa4lmsv2scg/Msa4LmsV2ScgApplication.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2scg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4LmsV2ScgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4LmsV2ScgApplication.class, args);
+    }
+
+}
